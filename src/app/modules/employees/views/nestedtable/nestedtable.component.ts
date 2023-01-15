a9b3afa873a60df1ce24fc4994c4c8839c858383
ismlslckk <ismlslck@gmail.com>
2023-01-15 18:17:16 +0100

    subtable still improving...

--- src/app/modules/employees/views/nestedtable/nestedtable.component.ts
+++ src/app/modules/employees/views/nestedtable/nestedtable.component.ts
@@ -11,7 +11,7 @@ export class NestedtableComponent implements OnInit {
 
   public items:Array<Employee>;
 
-  public itemsHeader:Array<string>;
+  public itemsHeader:Array<any>;
 
   constructor() { }
 
@@ -26,27 +26,50 @@ export class NestedtableComponent implements OnInit {
     const emp1= new Employee();
     emp1.firstName='emp1';
     emp1.lastName='last1';
+    emp1.email="wefwj@gmail.com";
+    emp1.departmentName="computer engineer";
+    emp1.phone='+905389784444';
+    emp1.position='senior';
 
     const emp11= new Employee();
     emp11.firstName='emp1_1';
     emp11.lastName='last1_1';
+    emp11.email="wefwj@gmail.com";
+    emp11.departmentName="computer engineer";
+    emp11.phone='+905389784444';
+    emp11.position='senior';
     
     const emp12= new Employee();
     emp12.firstName='emp1_2';
     emp12.lastName='last1_2';
+    emp12.email="wefwj@gmail.com";
+    emp12.departmentName="computer engineer";
+    emp12.phone='+905389784444';
+    emp12.position='senior';
 
     const emp2= new Employee();
     emp2.firstName='emp2';
     emp2.lastName='last2';
-
+    emp2.email="wefwj@gmail.com";
+    emp2.departmentName="computer engineer";
+    emp2.phone='+905389784444';
+    emp2.position='senior';
 
     const emp111= new Employee();
     emp111.firstName='emp1_1_1';
     emp111.lastName='last1_1_1';
+    emp111.email="wefwj@gmail.com";
+    emp111.departmentName="computer engineer";
+    emp111.phone='+905389784444';
+    emp111.position='senior';
     
     const emp122= new Employee();
     emp122.firstName='emp1_2_1';
     emp122.lastName='last1_2_1';
+    emp122.email="wefwj@gmail.com";
+    emp122.departmentName="computer engineer";
+    emp122.phone='+905389784444';
+    emp122.position='senior';
 
     emp11.subItems=[emp111,emp122,emp111,emp122];
 
@@ -55,7 +78,15 @@ export class NestedtableComponent implements OnInit {
     this.items=[emp1,emp2];
 
 
-    this.itemsHeader=['Ad','Soyad']
+    this.itemsHeader=
+    [
+      {header:'Name',columnName:'firstName'},
+      {header:'Surname',columnName:'lastName'},
+      {header:'E-mail',columnName:'email'},
+      {header:'Department',columnName:'departmentName'},
+      {header:'Phone',columnName:'phone'},
+      {header:'Position',columnName:'position'}
+    ]
 
   }
 
